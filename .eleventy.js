ded17afa706023292e1565b682d5357cb46bc97a
Felicia Tiffany Hertada <feliciatiffany@gmail.com>
2026-05-05 09:26:31 -0400

    Add pathPrefix for GitHub Pages subdirectory

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("scripts");
   eleventyConfig.addPassthroughCopy("images");
   return {
+    pathPrefix: "/wishlist-collection-eleventy/",
     dir: {
       input: ".",     // your source files
       output: "docs"  // GitHub Pages will serve from this
